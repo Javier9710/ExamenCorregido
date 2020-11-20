e01839649c6849a232b7844d24bdbfb8dd6857b3
unknown <javierandresmd@ufps.edu.co>
2020-11-19 23:21:03 -0500

    dasd

--- src/main/java/com/ufps/edu/controlador/RegistroComunidadController.java
+++ src/main/java/com/ufps/edu/controlador/RegistroComunidadController.java
@@ -9,11 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.examen.demo.entities.Visitante;
 import com.google.gson.Gson;
 import com.ufps.edu.model.dao.IBasicoDao;
 import com.ufps.edu.model.dao.IComorbilidadDao;
@@ -86,10 +89,7 @@ public class RegistroComunidadController {
 	}
 	
 	@PostMapping("registrarDatosPersonales")
-	public String registrarDatosPersonales(@RequestParam String documento, @RequestParam String nombre,
-			@RequestParam String fechaNacimiento, @RequestParam String telefono,
-			@RequestParam String tipoPersona, @RequestParam String eps, @RequestParam String genero,
-			@RequestParam(required = false, defaultValue = "false") String embarazo, @RequestParam String modalidad, Model model) throws ParseException {
+	public String registrarDatosPersonales(@ModelAttribute(value="visitante") Basico basico, RedirectAttributes r, Model model) throws ParseException {
 		Optional<Basico> opt=basicoDao.findByDocumento(documento);
 		System.out.println(embarazo);
 		if(opt.isPresent()) {
@@ -184,12 +184,7 @@ public class RegistroComunidadController {
 	}
 	
 	@PostMapping("registrarComorbilidades")
-	public String registrarComorbilidad(@RequestParam(required = false, defaultValue = "false") String 	diabetes,
-			@RequestParam(required = false, defaultValue = "false") String cardio,@RequestParam(required = false, defaultValue = "false") String cerebro,
-			@RequestParam(required = false, defaultValue = "false") String vih,@RequestParam(required = false, defaultValue = "false") String 	cancer,
-			@RequestParam(required = false, defaultValue = "false") String corticoides,@RequestParam(required = false, defaultValue = "false") String epoc,
-			@RequestParam(required = false, defaultValue = "false") String nutricion,@RequestParam(required = false, defaultValue = "false") String fumador,
-			@RequestParam String documento, Model model) {
+	public String registrarComorbilidad(@ModelAttribute(value="visitante") Comorbilidad comorbilidad, RedirectAttributes r, Model model) {
 		
 		Comorbilidad entity=new Comorbilidad();
 		Optional<Basico> opt=basicoDao.findByDocumento(documento);
